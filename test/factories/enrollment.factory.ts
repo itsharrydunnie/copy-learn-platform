9317658a6cf3372f24a7c307a6c16c583ceded51
Harry Dunnie <peteranietieh@gmail.com>
2026-09-07 16:29:40 +0100

    test: rebuild Pacepard factories

--- test/factories/enrollment.factory.ts
+++ test/factories/enrollment.factory.ts
@@ -1,15 +1,5 @@
 import { Types } from "mongoose";
 import Enrollment from "../../src/modules/core/enrollments/enroll.model";
-import { EnrollmentStatus, EnrollmentTargetType, EnrollmentType } from "../../src/modules/core/enrollments/enroll.interface";
-
-export const buildEnrollment = (overrides: Record<string, unknown> = {}) => ({
-  userId: new Types.ObjectId(),
-  targetType: EnrollmentTargetType.COURSE,
-  targetId: new Types.ObjectId(),
-  status: EnrollmentStatus.PENDING,
-  enrollmentType: EnrollmentType.REGULAR,
-  ...overrides,
-});
-
-export const createEnrollment = async (overrides: Record<string, unknown> = {}) =>
-  Enrollment.create(buildEnrollment(overrides));
+import { EnrollmentStatus,EnrollmentTargetType,EnrollmentType } from "../../src/modules/core/enrollments/enroll.interface";
+export const buildEnrollment=(overrides:Record<string,unknown>={})=>({userId:new Types.ObjectId(),targetType:EnrollmentTargetType.COURSE,targetId:new Types.ObjectId(),status:EnrollmentStatus.PENDING,enrollmentType:EnrollmentType.REGULAR,...overrides});
+export const createEnrollment=async(overrides:Record<string,unknown>={})=>Enrollment.create(buildEnrollment(overrides));
