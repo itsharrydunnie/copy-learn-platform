71239ec2a91b101201eb3c6f1b27fbf708722cca
Harry Dunnie <peteranietieh@gmail.com>
2026-09-07 16:29:47 +0100

    test: rebuild Pacepard factories

--- test/factories/transaction.factory.ts
+++ test/factories/transaction.factory.ts
@@ -1,24 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { Types } from "mongoose";
 import Transaction from "../../src/modules/payments/transaction/transaction.model";
-import { PaymentProvider, TransactionLabel, TransactionStatus, TransactionType } from "../../src/modules/payments/transaction/transaction.interface";
-
-export const buildTransaction = (overrides: Record<string, unknown> = {}) => ({
-  type: TransactionType.PAYMENT,
-  status: TransactionStatus.PENDING,
-  label: TransactionLabel.COURSE_PAYMENT,
-  description: "Course enrollment payment",
-  reference: faker.string.alphanumeric(16),
-  currency: "NGN",
-  amount: 6500000,
-  unitAmount: 65000,
-  providerName: PaymentProvider.PAYSTACK,
-  userId: new Types.ObjectId(),
-  enrollmentId: new Types.ObjectId(),
-  courseId: new Types.ObjectId(),
-  webhookProcessed: false,
-  ...overrides,
-});
-
-export const createTransaction = async (overrides: Record<string, unknown> = {}) =>
-  Transaction.create(buildTransaction(overrides));
+import { PaymentProvider,TransactionLabel,TransactionStatus,TransactionType } from "../../src/modules/payments/transaction/transaction.interface";
+export const buildTransaction=(overrides:Record<string,unknown>={})=>({type:TransactionType.PAYMENT,status:TransactionStatus.PENDING,label:TransactionLabel.COURSE_PAYMENT,description:"Course enrollment payment",reference:faker.string.alphanumeric(16),currency:"NGN",amount:6500000,unitAmount:65000,providerName:PaymentProvider.PAYSTACK,userId:new Types.ObjectId(),enrollmentId:new Types.ObjectId(),courseId:new Types.ObjectId(),webhookProcessed:false,...overrides});
+export const createTransaction=async(overrides:Record<string,unknown>={})=>Transaction.create(buildTransaction(overrides));
