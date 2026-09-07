48425786c0d4ca6d0b3d124e3ba0f6995a6a097b
Harry Dunnie <peteranietieh@gmail.com>
2026-09-07 16:29:23 +0100

    test: rebuild Pacepard factories

--- test/factories/program.factory.ts
+++ test/factories/program.factory.ts
@@ -1,18 +1,5 @@
 import { faker } from "@faker-js/faker";
 import Program from "../../src/modules/core/programs/program.model";
 import { ProgramStatus } from "../../src/modules/core/programs/program.dto";
-
-export const buildProgram = (overrides: Record<string, unknown> = {}) => ({
-  slug: faker.helpers.slugify(faker.company.name()).toLowerCase(),
-  title: faker.company.name(),
-  description: faker.lorem.paragraph(),
-  shortDescription: faker.lorem.sentence(),
-  thumbnail: faker.image.url(),
-  coverImage: faker.image.url(),
-  status: ProgramStatus.DRAFT,
-  enrollmentEnabled: true,
-  ...overrides,
-});
-
-export const createProgram = async (overrides: Record<string, unknown> = {}) =>
-  Program.create(buildProgram(overrides));
+export const buildProgram=(overrides:Record<string,unknown>={})=>({slug:faker.helpers.slugify(faker.company.name()).toLowerCase(),title:faker.company.name(),description:faker.lorem.paragraph(),shortDescription:faker.lorem.sentence(),thumbnail:faker.image.url(),coverImage:faker.image.url(),status:ProgramStatus.DRAFT,enrollmentEnabled:true,...overrides});
+export const createProgram=async(overrides:Record<string,unknown>={})=>Program.create(buildProgram(overrides));
