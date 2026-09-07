b037b6eb3c2ebecbc92438d7ee83b964a76c6c87
Harry Dunnie <peteranietieh@gmail.com>
2026-09-07 16:29:31 +0100

    test: rebuild Pacepard factories

--- test/factories/event.factory.ts
+++ test/factories/event.factory.ts
@@ -2,15 +2,5 @@ import { faker } from "@faker-js/faker";
 import { Types } from "mongoose";
 import Event from "../../src/modules/core/events/event.model";
 import { EventStatus } from "../../src/modules/core/events/event.interface";
-
-export const buildEvent = (overrides: Record<string, unknown> = {}) => ({
-  programId: new Types.ObjectId(),
-  title: faker.company.catchPhrase(),
-  description: faker.lorem.paragraph(),
-  scheduledAt: faker.date.future(),
-  status: EventStatus.DRAFT,
-  ...overrides,
-});
-
-export const createEvent = async (overrides: Record<string, unknown> = {}) =>
-  Event.create(buildEvent(overrides));
+export const buildEvent=(overrides:Record<string,unknown>={})=>({programId:new Types.ObjectId(),title:faker.company.catchPhrase(),description:faker.lorem.paragraph(),scheduledAt:faker.date.future(),status:EventStatus.DRAFT,...overrides});
+export const createEvent=async(overrides:Record<string,unknown>={})=>Event.create(buildEvent(overrides));
