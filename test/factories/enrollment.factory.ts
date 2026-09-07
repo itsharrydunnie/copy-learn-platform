beb637a2af84105aa790f4a65036916b6b46f859
Harry Dunnie <peteranietieh@gmail.com>
2026-09-07 16:28:56 +0100

    test: add Pacepard factory

---
+++ test/factories/enrollment.factory.ts
@@ -0,0 +1,15 @@
+import { Types } from "mongoose";
+import Enrollment from "../../src/modules/core/enrollments/enroll.model";
+import { EnrollmentStatus, EnrollmentTargetType, EnrollmentType } from "../../src/modules/core/enrollments/enroll.interface";
+
+export const buildEnrollment = (overrides: Record<string, unknown> = {}) => ({
+  userId: new Types.ObjectId(),
+  targetType: EnrollmentTargetType.COURSE,
+  targetId: new Types.ObjectId(),
+  status: EnrollmentStatus.PENDING,
+  enrollmentType: EnrollmentType.REGULAR,
+  ...overrides,
+});
+
+export const createEnrollment = async (overrides: Record<string, unknown> = {}) =>
+  Enrollment.create(buildEnrollment(overrides));
